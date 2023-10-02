4fa1ce68b539ccf1d5cc72834248306f3dab4791
Vijayan <141060363+Vijayanvictory@users.noreply.github.com>
2023-10-02 18:59:26 +0530

    Add files via upload

---
+++ Math function Task 3.js
@@ -0,0 +1,4 @@
+var a = [12,34,56,1];
+var b = [-12,-34,0,-56,-1];
+console.log(Math.max(...a));
+console.log(Math.max(...b));
---
+++ Math function Task 4.js
@@ -0,0 +1,4 @@
+var a = [12,34,56,1];
+var b = [-12,-34,0,-56,-1];
+console.log(Math.min(...a));
+console.log(Math.min(...b));
